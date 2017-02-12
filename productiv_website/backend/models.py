7372e73475b9de56b9631fa41243953ef673a16e
Joshua Richardson <joshua@richardson.tech>
2017-02-12 10:52:25 +0000

    Adds basis for models

--- productiv_website/backend/models.py
+++ productiv_website/backend/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    balance = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Website(models.Model):
+    url = models.TextField()
+    useful = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.username
