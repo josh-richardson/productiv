c19865a5a1574569ca31d9d1a185d62612def412
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2017-02-12 11:38:02 +0000

    Updated models again

--- productiv_website/backend/models.py
+++ productiv_website/backend/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     balance = models.DecimalField(default=0, max_digits=8, decimal_places=2)
-    sites = models.ManyToManyField("Website")
+    sites = models.ManyToManyField("Website", through="UserWebsite")
 
     def __str__(self):
         return self.user.username
@@ -18,9 +18,9 @@ class Website(models.Model):
     def __str__(self):
         return self.user.username
 
-class Preference(models.Model):
-    user =models.OneToOneField("UserProfile")
-    site = models.OneToOneField("Website")
+class UserWebsite(models.Model):
+    user =models.ForeignKey("UserProfile", on_delete=models.CASCADE)
+    site = models.ForeignKey("Website", on_delete=models.CASCADE)
     useful = models.BooleanField(default=True)
 
     def __str__(self):
