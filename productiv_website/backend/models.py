86c2e6c7dfc61dcf5df213bfe08e9676588ddd0a
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2017-02-12 11:03:50 +0000

    Updated models

--- productiv_website/backend/models.py
+++ productiv_website/backend/models.py
@@ -5,7 +5,8 @@ from django.db import models
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    balance = models.IntegerField(default=0)
+    balance = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    sites = models.ManyToManyField("Website")
 
     def __str__(self):
         return self.user.username
@@ -13,7 +14,14 @@ class UserProfile(models.Model):
 
 class Website(models.Model):
     url = models.TextField()
-    useful = models.BooleanField(default=True)
 
     def __str__(self):
         return self.user.username
+
+class Preference(models.Model):
+    user =models.OneToOneField("UserProfile")
+    site = models.OneToOneField("Website")
+    useful = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user + ": " + self.site + " - " + self.useful
